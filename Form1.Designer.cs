7bb486197f7f1d2ac1edbf9e5fa490eaf8bbc1a1
Farid Jamalov <faridjamalov006@gmail.com>
2026-06-03 18:38:36 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSize = new System.Windows.Forms.ComboBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.tableGame = new System.Windows.Forms.TableLayoutPanel();
+            this.lblMatches = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox5.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbSize
+            // 
+            this.cmbSize.FormattingEnabled = true;
+            this.cmbSize.Items.AddRange(new object[] {
+            "3x4",
+            "3x5"});
+            this.cmbSize.Location = new System.Drawing.Point(208, 38);
+            this.cmbSize.Name = "cmbSize";
+            this.cmbSize.Size = new System.Drawing.Size(127, 24);
+            this.cmbSize.TabIndex = 0;
+            this.cmbSize.SelectedIndexChanged += new System.EventHandler(this.cmbSize_SelectedIndexChanged);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.LightCoral;
+            this.btnUpload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(432, 22);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(150, 53);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "Upload Images";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(611, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(143, 54);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Game";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoves.Location = new System.Drawing.Point(19, 170);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(109, 29);
+            this.lblMoves.TabIndex = 5;
+            this.lblMoves.Text = "Moves: 0";
+            this.lblMoves.Click += new System.EventHandler(this.lblMoves_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(19, 103);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(160, 38);
+            this.lblTime.TabIndex = 6;
+            this.lblTime.Text = "Time: 00:00";
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 34);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Size Selection";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.btnStart);
+            this.groupBox5.Controls.Add(this.btnUpload);
+            this.groupBox5.Controls.Add(this.cmbSize);
+            this.groupBox5.Controls.Add(this.label1);
+            this.groupBox5.Location = new System.Drawing.Point(12, 10);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(765, 84);
+            this.groupBox5.TabIndex = 12;
+            this.groupBox5.TabStop = false;
+            // 
+            // tableGame
+            // 
+            this.tableGame.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableGame.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableGame.ColumnCount = 2;
+            this.tableGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.77712F));
+            this.tableGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.22288F));
+            this.tableGame.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableGame.Location = new System.Drawing.Point(31, 135);
+            this.tableGame.Name = "tableGame";
+            this.tableGame.RowCount = 2;
+            this.tableGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableGame.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableGame.Size = new System.Drawing.Size(591, 710);
+            this.tableGame.TabIndex = 3;
+            // 
+            // lblMatches
+            // 
+            this.lblMatches.AutoSize = true;
+            this.lblMatches.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatches.Location = new System.Drawing.Point(19, 33);
+            this.lblMatches.Name = "lblMatches";
+            this.lblMatches.Size = new System.Drawing.Size(128, 29);
+            this.lblMatches.TabIndex = 4;
+            this.lblMatches.Text = "Matches: 0";
+            this.lblMatches.Click += new System.EventHandler(this.lblMatches_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.groupBox1.Controls.Add(this.lblMoves);
+            this.groupBox1.Controls.Add(this.lblMatches);
+            this.groupBox1.Controls.Add(this.lblTime);
+            this.groupBox1.Location = new System.Drawing.Point(642, 288);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(322, 231);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(976, 845);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tableGame);
+            this.Controls.Add(this.groupBox5);
+            this.Name = "Form1";
+            this.Text = "Triple Match Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSize;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TableLayoutPanel tableGame;
+        private System.Windows.Forms.Label lblMatches;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<Image> selectedImages = new List<Image>();
+        List<Button> selectedCards = new List<Button>();
+        List<Image> cardImages = new List<Image>();
+
+        int matchesFound = 0;
+        int moves = 0;
+        int seconds = 0;
+
+        Timer gameTimer = new Timer();
+        Timer flipTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            gameTimer.Interval = 1000;
+            gameTimer.Tick += GameTimer_Tick;
+
+            flipTimer.Interval = 800;
+            flipTimer.Tick += FlipTimer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbSize.SelectedIndex = 0;
+
+            lblMatches.Text = "Matches: 0";
+            lblMoves.Text = "Moves: 0";
+            lblTime.Text = "Time: 00:00";
+
+            tableGame.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            lblTime.Text = "Time: " + TimeSpan.FromSeconds(seconds).ToString(@"mm\:ss");
+        }
+
+        private void FlipTimer_Tick(object sender, EventArgs e)
+        {
+            flipTimer.Stop();
+
+            foreach (var btn in selectedCards)
+            {
+                btn.BackgroundImage = null;
+                btn.Text = "?";
+                btn.Enabled = true;
+            }
+
+            selectedCards.Clear();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            int neededImages = cmbSize.SelectedItem.ToString() == "3x4" ? 4 : 5;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            ofd.Multiselect = true;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                if (ofd.FileNames.Length != neededImages)
+                {
+                    MessageBox.Show("You must select exactly " + neededImages + " images.");
+                    return;
+                }
+
+                selectedImages.Clear();
+
+                foreach (string file in ofd.FileNames)
+                {
+                    selectedImages.Add(Image.FromFile(file));
+                }
+
+                MessageBox.Show("Images uploaded successfully!");
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int rows = cmbSize.SelectedItem.ToString() == "3x4" ? 4 : 5;
+            int cols = 3;
+            int neededImages = rows == 4 ? 4 : 5;
+
+            if (selectedImages.Count != neededImages)
+            {
+                MessageBox.Show("First upload exactly " + neededImages + " images.");
+                return;
+            }
+
+            tableGame.Controls.Clear();
+            tableGame.RowStyles.Clear();
+            tableGame.ColumnStyles.Clear();
+
+            tableGame.RowCount = rows;
+            tableGame.ColumnCount = cols;
+
+            for (int i = 0; i < rows; i++)
+                tableGame.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / rows));
+
+            for (int i = 0; i < cols; i++)
+                tableGame.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / cols));
+
+            matchesFound = 0;
+            moves = 0;
+            seconds = 0;
+
+            lblMatches.Text = "Matches: 0";
+            lblMoves.Text = "Moves: 0";
+            lblTime.Text = "Time: 00:00";
+
+            cardImages.Clear();
+
+            foreach (Image img in selectedImages)
+            {
+                cardImages.Add(img);
+                cardImages.Add(img);
+                cardImages.Add(img);
+            }
+
+            Random rnd = new Random();
+            cardImages = cardImages.OrderBy(x => rnd.Next()).ToList();
+
+            for (int i = 0; i < cardImages.Count; i++)
+            {
+                Button card = new Button();
+                card.Dock = DockStyle.Fill;
+                card.Tag = cardImages[i];
+                card.Text = "?";
+                card.Font = new Font("Arial", 20, FontStyle.Bold);
+                card.Click += Card_Click;
+
+                tableGame.Controls.Add(card);
+            }
+
+            gameTimer.Start();
+            tableGame.Height = tableGame.Width;
+            this.Height = tableGame.Bottom + 120;
+        }
+
+        private void Card_Click(object sender, EventArgs e)
+        {
+            Button clickedCard = sender as Button;
+
+            if (selectedCards.Count == 3)
+                return;
+
+            clickedCard.BackgroundImage = (Image)clickedCard.Tag;
+            clickedCard.BackgroundImageLayout = ImageLayout.Stretch;
+            clickedCard.Text = "";
+            clickedCard.Enabled = false;
+
+            selectedCards.Add(clickedCard);
+
+            if (selectedCards.Count == 3)
+            {
+                moves++;
+                lblMoves.Text = "Moves: " + moves;
+
+                Image img1 = (Image)selectedCards[0].Tag;
+                Image img2 = (Image)selectedCards[1].Tag;
+                Image img3 = (Image)selectedCards[2].Tag;
+
+                if (img1 == img2 && img2 == img3)
+                {
+                    matchesFound++;
+                    lblMatches.Text = "Matches: " + matchesFound;
+
+                    selectedCards.Clear();
+
+                    int totalMatches = cmbSize.SelectedItem.ToString() == "3x4" ? 4 : 5;
+
+                    if (matchesFound == totalMatches)
+                    {
+                        gameTimer.Stop();
+
+                        MessageBox.Show(
+                            "YOU WIN!\nTime: " + TimeSpan.FromSeconds(seconds).ToString(@"mm\:ss") +
+                            "\nMoves: " + moves
+                        );
+                    }
+                }
+                else
+                {
+                    flipTimer.Start();
+                }
+            }
+            tableGame.Height = tableGame.Width;
+        }
+
+        private void lblMatches_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox4_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMoves_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
